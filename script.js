e5941e4426df357980fead04dbbd53fc618a5d31
rulalub <rulalub@hotmail.com>
2022-09-17 23:36:06 +0300

    adding <div class for the whole week

--- script.js
+++ script.js
@@ -4,8 +4,11 @@
 function displayForecast() {
 let forecastElement= document.querySelector(#forecast);
 
+let forecastHTML= '<div class="row">';
+forecastHTML= 
+forecastHTML + '
 
-forecastElement= innerHTML ="Forecast"; '
+
 <div class="row">
 
 <div class="col-md-2">
@@ -41,7 +44,76 @@ alt="cloudy" width="40px" />
 </div>';
 
 
+forecastElement= innerHTML ="Forecast"; '
+<div class="row">
 
-    
+<div class="col-md-2">
+  <div class="weather-forcast-date">Tue</div>
+                        
+<img src="https://th.bing.com/th/id/R.3ce25565e7de2175a08c7d3ee805c49c?rik=ILEGknNNlKZ5ow&pid=ImgRaw&r=0"
+alt="cloudy" width="40px" />
+
+
+<div class="weather-forcast-temperature">
+<span class="weather-forcast-temperature-max">19° </span>
+<span class="weather-forcast-temperature-min">15°</span>
+</div>
+</div> 
+</div>';
+
+
+ forecastElement= innerHTML ="Forecast"; '
+<div class="row">
+
+<div class="col-md-2">
+  <div class="weather-forcast-date">Tue</div>
+                        
+<img src="https://th.bing.com/th/id/R.3ce25565e7de2175a08c7d3ee805c49c?rik=ILEGknNNlKZ5ow&pid=ImgRaw&r=0"
+alt="cloudy" width="40px" />
+
+
+<div class="weather-forcast-temperature">
+<span class="weather-forcast-temperature-max">19° </span>
+<span class="weather-forcast-temperature-min">15°</span>
+</div>
+</div> 
+</div>';
+
+
+forecastElement= innerHTML ="Forecast"; '
+<div class="row">
+
+<div class="col-md-2">
+  <div class="weather-forcast-date">Tue</div>
+                        
+<img src="https://th.bing.com/th/id/R.3ce25565e7de2175a08c7d3ee805c49c?rik=ILEGknNNlKZ5ow&pid=ImgRaw&r=0"
+alt="cloudy" width="40px" />
+
+
+<div class="weather-forcast-temperature">
+<span class="weather-forcast-temperature-max">19° </span>
+<span class="weather-forcast-temperature-min">15°</span>
+</div>
+</div> 
+</div>';
+
+forecastElement= innerHTML ="Forecast"; '
+<div class="row">
+
+<div class="col-md-2">
+  <div class="weather-forcast-date">Tue</div>
+                        
+<img src="https://th.bing.com/th/id/R.3ce25565e7de2175a08c7d3ee805c49c?rik=ILEGknNNlKZ5ow&pid=ImgRaw&r=0"
+alt="cloudy" width="40px" />
+
+
+<div class="weather-forcast-temperature">
+<span class="weather-forcast-temperature-max">19° </span>
+<span class="weather-forcast-temperature-min">15°</span>
+</div>
+</div> 
+</div>';
+
+
 
 
