00ece4e3b68f39d036459736f72f3b4670468268
rulalub <rulalub@hotmail.com>
2022-09-17 23:19:06 +0300

    adding div class in script.js

---
+++ script.js
@@ -0,0 +1,47 @@
+
+
+
+function displayForecast() {
+let forecastElement= document.querySelector(#forecast);
+
+
+forecastElement= innerHTML ="Forecast"; '
+<div class="row">
+
+<div class="col-md-2">
+  <div class="weather-forcast-date">Tue</div>
+                        
+<img src="https://th.bing.com/th/id/R.3ce25565e7de2175a08c7d3ee805c49c?rik=ILEGknNNlKZ5ow&pid=ImgRaw&r=0"
+alt="cloudy" width="40px" />
+
+
+<div class="weather-forcast-temperature">
+<span class="weather-forcast-temperature-max">19° </span>
+<span class="weather-forcast-temperature-min">15°</span>
+</div>
+</div> 
+</div>';
+
+
+forecastElement= innerHTML ="Forecast"; '
+<div class="row">
+
+<div class="col-md-2">
+  <div class="weather-forcast-date">Tue</div>
+                        
+<img src="https://th.bing.com/th/id/R.3ce25565e7de2175a08c7d3ee805c49c?rik=ILEGknNNlKZ5ow&pid=ImgRaw&r=0"
+alt="cloudy" width="40px" />
+
+
+<div class="weather-forcast-temperature">
+<span class="weather-forcast-temperature-max">19° </span>
+<span class="weather-forcast-temperature-min">15°</span>
+</div>
+</div> 
+</div>';
+
+
+
+    
+
+
